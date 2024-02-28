1173434ae831d210ef67707b27d8c7741f877367
AhmedSamy9828 <ahmed28samy98@gmail.com>
2024-02-28 23:29:07 +0200

    FIX {get_line.c}

--- get_line.c
+++ get_line.c
@@ -1,19 +1,25 @@
 #include "shell.h"
 
 /**
- * bring_line - Assigns the line buffer for get_line.
- *
- * This function is responsible for assigning the line buffer for the get_line
- * function based on the input parameters.
- *
- * @lineptr: Pointer to the buffer that stores the input string.
- * @n: Pointer to the size of the line buffer.
- * @buffer: Pointer to the string that is being assigned to lineptr.
- * @j: Size of the buffer.
+ * bring_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
  */
 void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
-	if (*lineptr == NULL || *n < j)
+
+	if (*lineptr == NULL)
+	{
+		if  (j > BUFSIZE)
+			*n = j;
+
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else if (*n < j)
 	{
 		if (j > BUFSIZE)
 			*n = j;
@@ -27,18 +33,12 @@ void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 		free(buffer);
 	}
 }
-
 /**
- * get_line - Reads input from stream.
- *
- * This function reads input from the specified stream and stores it in the
- * provided buffer.
- *
- * @lineptr: Pointer to the buffer that stores the input.
- * @n: Pointer to the size of the lineptr buffer.
- * @stream: Stream to read from.
- *
- * Return: The number of bytes read, or -1 on failure.
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
  */
 ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 {
@@ -48,14 +48,15 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 	char *buffer;
 	char t = 'z';
 
-	if (input != 0)
+	if (input == 0)
+		fflush(stream);
+	else
 		return (-1);
-
 	input = 0;
-	buffer = malloc(sizeof(char) * BUFSIZE);
-	if (buffer == NULL)
-		return (-1);
 
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
+		return (-1);
 	while (t != '\n')
 	{
 		i = read(STDIN_FILENO, &t, 1);
@@ -74,13 +75,10 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 		buffer[input] = t;
 		input++;
 	}
-
 	buffer[input] = '\0';
 	bring_line(lineptr, n, buffer, input);
 	retval = input;
 	if (i != 0)
 		input = 0;
-
 	return (retval);
 }
-
