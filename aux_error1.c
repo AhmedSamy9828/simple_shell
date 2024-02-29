b7c461e29a69a041b0736bf61c5430aa2f7b1611
AhmedSamy9828 <ahmed28samy98@gmail.com>
2024-02-29 02:09:45 +0200

    aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -7,6 +7,7 @@
  * @msg: message to print
  * @error: output message
  * @ver_str: counter lines
+ *
  * Return: error message
  */
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
@@ -40,7 +41,9 @@ char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 
 /**
  * error_get_cd - error message for cd command in get_cd
+ *
  * @datash: data relevant (directory)
+ *
  * Return: Error message
  */
 char *error_get_cd(data_shell *datash)
@@ -79,7 +82,9 @@ char *error_get_cd(data_shell *datash)
 
 /**
  * error_not_found - generic error message for command not found
+ *
  * @datash: data relevant (counter, arguments)
+ *
  * Return: Error message
  */
 char *error_not_found(data_shell *datash)
@@ -111,6 +116,7 @@ char *error_not_found(data_shell *datash)
 
 /**
  * error_exit_shell - generic error message for exit in get_exit
+ *
  * @datash: data relevant (counter, arguments)
  *
  * Return: Error message
