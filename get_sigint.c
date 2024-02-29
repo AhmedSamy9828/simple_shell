0ffde6c4708e82131db0abaddec724ccaf23dd21
AhmedSamy9828 <ahmed28samy98@gmail.com>
2024-02-29 02:04:51 +0200

    get_sigint.c

--- get_sigint.c
+++ get_sigint.c
@@ -2,10 +2,13 @@
 
 /**
  * get_sigint - Handle the crtl + c call in prompt
+ *
  * @sig: Signal handler
+ *
+ * Return: Nothing
  */
 void get_sigint(int sig)
 {
 	(void)sig;
-	write(STDOUT_FILENO, "\n^-^ ", 5);
+	write(STDOUT_FILENO, "\n$ ", 3);
 }
