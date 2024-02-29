03bcb8579f1c6aad57bacd6659b3408e82c3020e
AhmedSamy9828 <ahmed28samy98@gmail.com>
2024-02-29 02:05:06 +0200

    get_line.c

--- get_line.c
+++ get_line.c
@@ -2,10 +2,13 @@
 
 /**
  * bring_line - assigns the line var for get_line
+ *
  * @lineptr: Buffer that store the input str
  * @buffer: str that is been called to line
  * @n: size of line
  * @j: size of buffer
+ *
+ * Return: nothing
  */
 void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
@@ -33,11 +36,14 @@ void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 		free(buffer);
 	}
 }
+
 /**
  * get_line - Read inpt from stream
+ *
  * @lineptr: buffer that stores the input
  * @n: size of lineptr
  * @stream: stream to read from
+ *
  * Return: The number of bytes
  */
 ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
